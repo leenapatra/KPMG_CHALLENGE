5b0936164e3ee5a4d040e85e192665d8e796d18b
leenapatra <78405009+leenapatra@users.noreply.github.com>
2021-06-20 19:52:58 +0100

    Update Challenge_2_-metadata.sh

--- Challenge_2_-metadata.sh
+++ Challenge_2_-metadata.sh
@@ -1,8 +1,15 @@
+#This Script has to be executed from an EC2 Instance. The URL Mentioned below is the one that returns the instance metadata
+
+#line 4 to line 7 gets the instance metadata and stores in an array for processing
 metadata_url='http://169.254.169.254/latest/meta-data/'
 metadata=`curl -s $metadata_url`
 meta_arr=( $metadata )
 meta_len=${#meta_arr[@]}
+
+#remove any existing file and then process
 rm -rf  myfile.json
+
+#start building your json.
 echo '{' > myfile.json
 i=0
 for x in $metadata; do
@@ -17,3 +24,5 @@ for x in $metadata; do
 done
 
 echo '}' >> myfile.json
+
+cat myfile.json
